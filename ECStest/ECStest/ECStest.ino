c05b175b4a6bdbf93fca022cba94a231fa4d2a21
Oliver Perez Fonolleras <opf1u20@soton.ac.uk>
2022-02-21 15:02:35 +0000

    program modified to measure thrust over current using a potentiometer

--- ECStest/ECStest/ECStest.ino
+++ ECStest/ECStest/ECStest.ino
@@ -4,16 +4,24 @@
 Servo ESC;     // create servo object to control the ESC
 
 int potValue;  // value from the analog pin
-
+double thrustPercent;
 void setup() {
   // Attach the ESC on pin 9
   ESC.attach(9,1000,2000); // (pin, min pulse width, max pulse width in microseconds) 
+  Serial.begin(9600);
 }
 
 void loop() {
- // potValue = analogRead(A0);   // reads the value of the potentiometer (value between 0 and 1023)
-  //potValue = map(potValue, 0, 1023, 0, 180);   // scale it to use it with the servo library (value between 0 and 180)
+  potValue = analogRead(A0);   // reads the value of the potentiometer (value between 0 and 1023)
+      Serial.print("Pot value = ");Serial.println(potValue);
+        thrustPercent = (double(potValue)/1023)*100;
+
+ potValue = map(potValue, 0, 1023, 0, 180);   // scale it to use it with the servo library (value between 0 and 180)
 
   //For continuous servo it rotates right if 180 degrees and left if 0 degrees.
-  ESC.write(0);    // Send the signal to the ESC
+  ESC.write(potValue);    // Send the signal to the ESC
+
+
+  Serial.print("Current thrust = ");Serial.println(thrustPercent);
+  delay(100);
 }
