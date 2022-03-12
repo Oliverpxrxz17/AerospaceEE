a6b2fe5f5bf66642a926f2faa008d9a8720f9aeb
Oliver Perez Fonolleras <opf1u20@soton.ac.uk>
2022-03-12 14:37:50 +0000

    modification to print dframedata

--- ThermalCamera/ThermalTest1.py
+++ ThermalCamera/ThermalTest1.py
@@ -11,13 +11,14 @@ mlx = adafruit_mlx90640.MLX90640(i2c) # begin MLX90640 with I2C comm
 mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ # set refresh rate
 
 frame = np.zeros((24*32,)) # setup array for storing all 768 temperatures
-while True:
+    while True:
     try:
-        mlx.getFrame(frame) # read MLX temperatures into frame var
+        print(mlx.getFrame(frame))
+        #mlx.getFrame(frame) # read MLX temperatures into frame var
         break
     except ValueError:
         continue # if error, just read again
 
 # print out the average temperature from the MLX90640
-print('Average MLX90640 Temperature: {0:2.1f}C ({1:2.1f}F)'.\
-      format(np.mean(frame),(((9.0/5.0)*np.mean(frame))+32.0)))
+#print('Average MLX90640 Temperature: {0:2.1f}C ({1:2.1f}F)'.\
+#     format(np.mean(frame),(((9.0/5.0)*np.mean(frame))+32.0)))
