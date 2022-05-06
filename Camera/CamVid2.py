8100086841755e00a43a609d7c32e2e9d4d9e17b
Oliverpxrxz17 <opf1u20@soton.ac.uk>
2022-05-06 14:38:18 +0100

    videoAutonomous

--- Camera/CamVid2.py
+++ Camera/CamVid2.py
@@ -2,6 +2,6 @@ import picamera
 
 camera = picamera.PiCamera()
 camera.resolution = (640, 480)
-camera.start_recording('flightTestStabiliseMode2.h264')
+camera.start_recording('flightTestAutonomous2.h264')
 camera.wait_recording(120)
 camera.stop_recording()
