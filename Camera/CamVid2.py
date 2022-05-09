8aa2fdf72a3eaafa01fcc76b893d3e05c1cfc7cc
Oliverpxrxz17 <opf1u20@soton.ac.uk>
2022-05-09 11:46:07 +0100

    camvid2

--- Camera/CamVid2.py
+++ Camera/CamVid2.py
@@ -1,6 +1,15 @@
 import picamera
 
+
+RESOLUTION = (1280, 720)
+FRAMERATE = 30
+
 camera = picamera.PiCamera()
+camera.resolution = RESOLUTION
+camera.framerate = FRAMERATE
+camera.exposure_mode = 'sports'
+
+
 camera.resolution = (640, 480)
 camera.start_recording('flightTestAutonomous2.h264')
 camera.wait_recording(120)
