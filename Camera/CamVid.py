bea9fba8027977927b381f6bdf745965ad7036f5
Oliver Perez Fonolleras <opf1u20@soton.ac.uk>
2022-05-09 12:05:49 +0100

    morerecording

--- Camera/CamVid.py
+++ Camera/CamVid.py
@@ -1,8 +1,15 @@
 import picamera
 with picamera.PiCamera() as camera:
     for filename in camera.record_sequence([
-            'clip01.h264',
-            'clip02.h264',
-            'clip03.h264']):
+            'flight_09_05_11.h264',
+            'flight_09_05_12.h264',
+            'flight_09_05_13.h264',
+            'flight_09_05_14.h264',
+            'flight_09_05_15.h264',
+            'flight_09_05_16.h264',
+            'flight_09_05_17.h264',
+            'flight_09_05_18.h264',
+            'flight_09_05_19.h264',
+            'flight_09_05_110.h264']):
         print('Recording to %s' % filename)
         camera.wait_recording(10)
